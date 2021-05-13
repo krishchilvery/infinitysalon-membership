9aebd5d3f5f1ca82469e4ff347cca27e4678f9ea
Krish Chilvery <saikrishnachilvery@gmail.com>
2021-05-13 12:22:18 +0530

    Added firebase hosting integration

--- client.js
+++ client.js
@@ -1,8 +1,8 @@
 const path = require('path');
-
+const http = require('http');
 const { app, BrowserWindow } = require('electron');
 const isDev = require('electron-is-dev');
-const {exec} = require('child_process')
+const handler = require('serve-handler');
 
 function createWindow() {
   // Create the browser window.
@@ -11,12 +11,18 @@ function createWindow() {
       nodeIntegration: true,
     },
   });
-
-  exec("npm run start-prod")
   // and load the index.html of the app.
   // win.loadFile("index.html");
   win.removeMenu()
-  win.loadURL('http://localhost:3000');
+  if(isDev){
+    try{
+      win.loadURL('http://localhost:5000');
+    }catch(err){
+      console.log(err)
+    }
+  }else{
+    win.loadURL('http://localhost:3000')
+  }
   // Open the DevTools.
   if (isDev) {
     win.webContents.openDevTools({ mode: 'detach' });
@@ -24,15 +30,40 @@ function createWindow() {
   win.maximize()
 }
 
+const server = http.createServer((request, response) => {
+  return handler(request, response,
+      {
+        public: path.join(__dirname, 'build'),
+        cleanUrls: true
+      }
+    )
+})
+
+function startServer(){
+  server.listen(5000, ()=>{
+    console.log('Running at port 5000');
+  })
+}
+
+function stopServer(){
+  server.removeAllListeners();
+}
+
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.whenReady().then(createWindow);
+app.whenReady().then(
+  () => {
+    startServer();
+    createWindow();
+  }
+);
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
+  stopServer();
   if (process.platform !== 'darwin') {
     app.quit();
   }
